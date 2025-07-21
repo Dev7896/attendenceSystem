6885a349a4f520fc88915ada0239b2a6ff3411e3
Dev7896 <barmandev402@gmail.com>
2025-07-21 13:11:28 +0530

    changes

--- config.py
+++ config.py
@@ -1,9 +1,8 @@
 import os
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'shivam_attendnce_models')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL') 
+    SECRET_KEY = os.getenv('SECRET_KEY')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
     UPLOAD_FOLDER = os.path.join(os.getcwd(), 'app', 'static', 'uploads')
     MAX_CONTENT_LENGTH = 16 * 1024 * 1024
